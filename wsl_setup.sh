1ca135c3dc062ea0d9e61246c28672e3a68ab019
Isaac Qing Guang Pang <isaac.pang91@gmail.com>
2020-01-26 09:53:34 +1100

    Force -y and autoremove

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -1,5 +1,5 @@
-sudo apt update && sudo apt upgrade
-sudo apt install vim-gtk neovim
-sudo apt install nodejs yarn
+sudo apt update && sudo apt upgrade -y
+sudo apt install vim-gtk neovim nodejs yarn -y
+sudo apt autoremove -y
 mkdir /mnt/d/WSL\ Projects
 cd ~ && ln -s /mnt/d/WSL\ Projects
